9bd4258fd07e170a1d149c5c188aa34be1d95086
maverick <115067830+sarcasticoutcast@users.noreply.github.com>
2025-02-21 04:20:35 +0530

    Add files via upload

---
+++ conditional.java
@@ -0,0 +1,11 @@
+public class conditional {
+    public static void main(String args[]) {
+        int a = 1;
+        int b = 5;
+        if (a >= b) {
+            System.out.println("a is largest of 2");
+        } else {
+            System.out.println("b is the largest of 2");
+        }
+    }
+}
